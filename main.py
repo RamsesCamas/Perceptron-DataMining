0a56895e9353ac8a8e04d69b769dd6c431f8f7a0
RamsesCamas <machiniram@gmail.com>
2022-07-06 00:41:07 -0500

    Perceptron class finished, data split and prediction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from perceptron import Perceptron
 
 def extract_data(filename):
     inputs = []
@@ -9,7 +10,7 @@ def extract_data(filename):
             data = line.replace('\n','').split(',')
             input = data[0:4]
             output = data[4]
-            input = list(map(float,input))
+            input = list(map(lambda x: float(x)/10,input))
             inputs.append(input)
             outputs.append(output)
     return inputs, outputs
@@ -20,15 +21,29 @@ def transform_outputs(output):
 def generate_weights(input):
     initial_weights = []
     for _ in range(len(input)):
-        weight_n = random.uniform(0,1)
+        weight_n = random.uniform(-1,1)
         initial_weights.append(weight_n)
     return initial_weights
 
+def split_data(inputs,outputs):
+    setosa_input_data = inputs[0:len(inputs)//2] 
+    setosa_output_data = outputs[0:len(inputs)//2] 
+    versi_input_data = inputs[len(inputs)//2:len(inputs)] 
+    versi_output_data = outputs[len(inputs)//2:len(inputs)] 
 
+    test_data = setosa_input_data[0:10] + versi_input_data[0:10]
+    train_input_data = setosa_input_data[10:len(setosa_input_data)] + versi_input_data[10:len(versi_output_data)]
+    train_output_data = setosa_output_data[10:len(setosa_output_data)] + versi_output_data[10:len(versi_output_data)]
+
+    return train_input_data, train_output_data, test_data
 
 if __name__ == '__main__':
     inputs, outputs = extract_data('iris.data')
-    transformed_outputs = list(map(transform_outputs,outputs))
+    train_input, train_output, test_data = split_data(inputs,outputs)
+    transformed_outputs = list(map(transform_outputs,train_output))
     learning_rate = random.uniform(0,1)
-    initial_weights = generate_weights(inputs[0])
-    print(initial_weights)
+    initial_weights = generate_weights(train_input[0])
+    perceptron = Perceptron(train_input,initial_weights, transformed_outputs,learning_rate)
+    final_weights = perceptron.train()
+    perceptron_2 = Perceptron(test_data,final_weights)
+    perceptron_2.predict(test_data)
--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,42 @@
 class Perceptron:
 
-    def __init__(self,inputs,outputs, weights) -> None:
+    def __init__(self,inputs, weights, outputs=None, learning_rate=0) -> None:
         self.inputs = inputs
         self.outputs = outputs
         self.weights = weights
+        self.learning_rate = learning_rate
         self.threshold = 0
 
-    def dot_product():
-        pass
+    def dot_product(self,input):
+        y = 0
+        for x_n, w_n in zip(input,self.weights):
+            y += x_n * w_n
+        return y
 
-    def train():
-        pass
-    
+    def step_function(self, y):
+        return 1 if y >= 0 else -1
+
+    def train(self):
+        contador = 0
+        while contador < len(self.inputs):
+            y = self.dot_product(self.inputs[contador])
+            y = self.step_function(y)
+            error = self.outputs[contador] - y
+            if error == 0:
+                contador += 1
+            else:
+                for i, w in enumerate(self.weights):
+                    self.weights[i] = w + (self.learning_rate * error * self.inputs[contador][i])
+                contador = 0
+        return self.weights
+
+    def predict(self,inputs):
+        contador = 0
+        while contador < len(inputs):
+            y = self.dot_product(inputs[contador])
+            y = self.step_function(y)
+            if y == 1:
+                print('Iris-setosa')
+            else:
+                print('Iris-versicolor')
+            contador += 1
